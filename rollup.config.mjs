663f37f792d5081a9e77742b743cec9d3d36ec53
Susan Cheng <susan.doggie@gmail.com>
2025-04-03 12:56:25 +0800

    test

---
+++ rollup.config.mjs
@@ -0,0 +1,80 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
+import dts from 'rollup-plugin-dts';
+
+const rollupConfig = {
+  input: {
+    index: 'src/index',
+  },
+  external: [
+    /node_modules/
+  ],
+  makeAbsoluteExternalsRelative: true,
+};
+
+const resolvePlugin = resolve({
+  extensions: [
+    '.tsx', '.jsx', '.ts', '.mjs', '.js',
+  ]
+});
+
+const rollupPlugins = [
+  typescript({
+    declaration: false,
+    exclude: ['tests/**/*'],
+  }),
+  babel({
+    babelrc: false,
+    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+  }),
+  commonjs({
+    transformMixedEsModules: true,
+  }),
+  json(),
+];
+
+export default [
+  {
+    ...rollupConfig,
+    output: [
+      {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'internals/[name]-[hash].js',
+        dir: './dist',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        entryFileNames: '[name].mjs',
+        chunkFileNames: 'internals/[name]-[hash].mjs',
+        dir: './dist',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolvePlugin,
+      ...rollupPlugins
+    ],
+  },
+  {
+    ...rollupConfig,
+    output: [
+      {
+        entryFileNames: '[name].d.ts',
+        chunkFileNames: 'internals/[name]-[hash].d.ts',
+        dir: './dist',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolvePlugin,
+      dts()
+    ],
+  },
+];
