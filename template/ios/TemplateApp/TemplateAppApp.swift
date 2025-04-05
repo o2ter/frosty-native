43e14c4c8e680faec61750852e8995dac739ae48
Susan Cheng <susan.doggie@gmail.com>
2025-04-05 15:46:59 +0800

    test

---
+++ template/ios/TemplateApp/TemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateAppApp.swift
+//  TemplateApp
+//
+//  Created by Susan Cheng on 5/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct TemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ template/ios/TemplateAppTests/TemplateAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TemplateAppTests.swift
+//  TemplateAppTests
+//
+//  Created by Susan Cheng on 5/4/2025.
+//
+
+import Testing
+
+struct TemplateAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
