62c03bca002fc74a655e7d094e280625dfbfec63
Susan Cheng <susan.doggie@gmail.com>
2025-04-07 22:00:03 +0800

    Update webpack.config.js

--- core-js/webpack.config.js
+++ core-js/webpack.config.js
@@ -1,10 +1,11 @@
 const webpack = require('webpack')
 
 module.exports = {
-  context: __dirname + "/src",
-  entry: "./index.mjs",
+  context: __dirname + '/src',
+  entry: './index.mjs',
+  mode: 'production',
   output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js"
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
   }
 }
