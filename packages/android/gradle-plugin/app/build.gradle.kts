713db3f6db5dd9ac137258648bd18302ed554670
Susan Cheng <susan.doggie@gmail.com>
2025-04-21 11:21:05 +0800

    Update build.gradle.kts

--- packages/android/gradle-plugin/app/build.gradle.kts
+++ packages/android/gradle-plugin/app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
+
+android {
+    namespace = "com.o2ter"
+    compileSdk = 34
+}
+
 dependencies {
     implementation(libs.androidx.javascriptengine)
 }
