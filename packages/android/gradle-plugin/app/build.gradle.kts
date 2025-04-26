62b87818208e5e683554e2dd313b70c3491aae42
Susan Cheng <susan.doggie@gmail.com>
2025-04-26 20:36:38 +0800

    Update build.gradle.kts

--- packages/android/gradle-plugin/app/build.gradle.kts
+++ packages/android/gradle-plugin/app/build.gradle.kts
@@ -28,23 +28,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
 }
 
-android {
-    namespace = "com.o2ter"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.o2ter"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-}
-
 dependencies {
     implementation(libs.androidx.javascriptengine)
 }
