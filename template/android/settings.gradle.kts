8beb76d6c7193978520484a6b1943c95421b8335
Susan Cheng <susan.doggie@gmail.com>
2025-05-07 19:46:56 +0800

    Update settings.gradle.kts

--- template/android/settings.gradle.kts
+++ template/android/settings.gradle.kts
@@ -22,4 +22,8 @@ dependencyResolutionManagement {
 
 rootProject.name = "TemplateApp"
 include(":app")
-includeBuild("../node_modules/frosty-native/packages/android/gradle-plugin")
+includeBuild("../node_modules/frosty-native/packages/android/gradle-plugin") {
+    dependencySubstitution {
+        substitute(module("com.o2ter:frosty-native-gradle-plugin")).using(project(":frosty-native-gradle-plugin"))
+    }
+}
