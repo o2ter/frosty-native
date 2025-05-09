480c7182e9b49c5a2861ba59efafcc49f9be961a
Susan Cheng <susan.doggie@gmail.com>
2025-05-09 21:50:27 +0800

    Update build.gradle.kts

--- packages/android/gradle-plugin/app/build.gradle.kts
+++ packages/android/gradle-plugin/app/build.gradle.kts
@@ -43,6 +43,13 @@ android {
             useSupportLibrary = true
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
