c0b2a94f2e9d6b96ca2d9aa881681d812fb4557c
Susan Cheng <susan.doggie@gmail.com>
2025-10-07 18:41:41 +0800

    Update build.gradle.kts

--- packages/android/gradle-plugin/core/build.gradle.kts
+++ packages/android/gradle-plugin/core/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.savedstate)
-    implementation(libs.o2ter.jscore)
+    implementation("com.o2ter:jscore-android")
 }
 
 tasks.preBuild {
