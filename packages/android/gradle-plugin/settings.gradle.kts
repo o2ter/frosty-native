0023d514f61d9014e5bbfbd108ce0cea015f7d64
Susan Cheng <susan.doggie@gmail.com>
2025-11-22 17:53:52 +0800

    clean

--- packages/android/gradle-plugin/settings.gradle.kts
+++ packages/android/gradle-plugin/settings.gradle.kts
@@ -58,7 +58,7 @@ gitRepositories {
     checkoutsDirectory.set(file(".checkouts"))
     include("jscore") {
         uri.set("https://github.com/o2ter/JSCore.git")
-        tag.set("0.0.22")
+        tag.set("0.0.23")
     }
 }
 
